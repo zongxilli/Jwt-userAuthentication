e5ea2085d584982d4ba1c9da93c3e67124a3cf0d
zongxilli <zongxi2014@gmail.com>
2021-07-12 18:20:57 -0400

    set up React-Router-Dom

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,60 @@
-import React, { Fragment } from 'react';
+import React, { Fragment, useState } from 'react';
 import './App.css';
+import { BrowserRouter, Switch, Route, Redirect } from 'react-router-dom';
+import Dashboard from './components/Dashboard';
+import Login from './components/Login';
+import Register from './components/Register';
 
 function App() {
-	return <Fragment></Fragment>;
+	const [isAuthenticated, setIsAuthenticated] = useState(false);
+
+	const setAuth = (boolean) => {
+		setIsAuthenticated(boolean);
+	};
+
+	return (
+		<Fragment>
+			<BrowserRouter>
+				<div className="container">
+					<Switch>
+						<Route
+							exact
+							path="/login"
+							render={(props) =>
+								!isAuthenticated ? (
+									<Login {...props} setAuth={setAuth} />
+								) : (
+									<Redirect to="/dashboard" />
+								)
+							}
+						/>
+						<Route
+							exact
+							path="/register"
+							render={(props) =>
+								!isAuthenticated ? (
+									<Register {...props} setAuth={setAuth} />
+								) : (
+									<Redirect to="/login" />
+								)
+							}
+						/>
+						<Route
+							exact
+							path="/dashboard"
+							render={(props) =>
+								isAuthenticated ? (
+									<Dashboard {...props} setAuth={setAuth} />
+								) : (
+									<Redirect to="/login" />
+								)
+							}
+						/>
+					</Switch>
+				</div>
+			</BrowserRouter>
+		</Fragment>
+	);
 }
 
 export default App;
